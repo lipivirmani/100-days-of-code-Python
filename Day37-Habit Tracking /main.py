82c98953e726b19a246335ddb16c31fe333227eb
lipivirmani <84458173+lipivirmani@users.noreply.github.com>
2025-01-16 23:41:30 +0530

    main.py

--- Day37-Habit Tracking /main.py
+++ Day37-Habit Tracking /main.py
@@ -1,9 +1,9 @@
 import requests
 from datetime import datetime
 
-USERNAME = "YOUR USERNAME"
-TOKEN = "YOUR SELF GENERATED TOKEN"
-GRAPH_ID = "YOUR GRAPH ID"
+USERNAME = "Lipi"
+TOKEN = "adhbkfeoe8iekrt9"
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -21,11 +21,11 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": GRAPH_ID,
-    "name": "Cycling Graph",
-    "unit": "Km",
+    "id": "graph1",
+    "name": "Number of hours studied",
+    "unit": "Hours",
     "type": "float",
-    "color": "ajisai"
+    "color": "sora"
 }
 
 headers = {
@@ -33,7 +33,6 @@ headers = {
 }
 
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-# print(response.text)
 
 pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 
@@ -42,7 +41,7 @@ today = datetime.now()
 
 pixel_data = {
     "date": today.strftime("%Y%m%d"),
-    "quantity": input("How many kilometers did you cycle today? "),
+    "quantity": input("How many hours did you study today? "),
 }
 
 response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
