aa654a1a7dace5e380c653695ae4a4ece2497aee
lipivirmani <84458173+lipivirmani@users.noreply.github.com>
2025-04-19 22:44:28 +0530

    Update main.py

--- Day67- Blog Post/main.py
+++ Day67- Blog Post/main.py
@@ -9,21 +9,9 @@ from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 from datetime import date
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = ''
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
